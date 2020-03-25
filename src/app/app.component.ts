fa3d9716df5f06edcf3e376dce342a9b5338b986
Gourav Agrawal (Student) <gagrawa3@asurite.asu.edu>
2020-03-25 02:13:30 -0700

    Added File Upload functionality. Integrated with S3.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,135 @@
 import { Component } from '@angular/core';
+import { Storage } from 'aws-amplify';
 import { AmplifyService } from 'aws-amplify-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  amplifyService: AmplifyService;
+  signedIn: boolean;
+  user: any;
+  show: boolean = false;
+  greeting: string;
+  files: any[] = [];
+  usernameAttributes = "email"; 
+  signUpConfig = {
+    header: 'Create a new account',
+    hideAllDefaults: true,
+    defaultCountryCode: '1',
+    signUpFields: [
+      {
+        label: 'Name',
+        key: 'name',
+        required: true,
+        displayOrder: 1,
+        type: 'string'
+      },
+      {
+        label: 'Email',
+        key: 'email',
+        required: true,
+        displayOrder: 1,
+        type: 'string',
+      },
+      {
+        label: 'Password',
+        key: 'password',
+        required: true,
+        displayOrder: 2,
+        type: 'password'
+      },
+      {
+        label: 'Phone Number',
+        key: 'phone_number',
+        required: true,
+        displayOrder: 3,
+        type: 'string'
+      }
+    ]
+  };
+
   constructor(public amplify: AmplifyService) {
+    this.amplifyService = amplify;
+    // this.Storage = this.amplifyService.storage();
     amplify.auth().currentAuthenticatedUser().then(console.log)
+    this.amplifyService.authStateChange$
+      .subscribe(authState => {
+        this.signedIn = authState.state === 'signedIn';
+        if (!authState.user) {
+          this.user = null;
+        } else {
+          debugger
+          this.user = authState.user;
+          this.greeting = "Hello " + this.user.username;
+          this.displayAllFiles();
+        }
+      });
+  }
+
+  displayAllFiles() {
+    Storage.list('')
+    .then(result => {
+      this.files = result;
+      console.log(this.files[0]);
+    })
+    .catch(err => console.log(err));
+  }
+
+  /**
+   * on file drop handler
+   */
+  onFileDropped($event) {
+    this.prepareFilesList($event);
+  }
+
+  /**
+   * handle file upload
+   */
+  fileUploadHandler(files) {
+    this.prepareFilesList(files);
+  }
+
+  /**
+   * Upload process
+   */
+  uploadFilesToS3(file: File) {
+    Storage.put(file.name, file)
+        .then (result => {
+          console.log(result)
+          this.displayAllFiles();
+        })
+        .catch(err => console.log(err));
+  }
+
+  /**
+   * Convert Files list to normal array list
+   * @param files (Files List)
+   */
+  prepareFilesList(files: Array<any>) {
+    for (const item of files) {
+      // this.files.push(item);
+      this.uploadFilesToS3(item);
+    }
+    // this.uploadFilesToS3();
+    this.displayAllFiles();
+  }
+
+  /**
+   * format bytes
+   * @param bytes (File size in bytes)
+   * @param decimals (Decimals point)
+   */
+  formatBytes(bytes, decimals) {
+    if (bytes === 0) {
+      return '0 Bytes';
+    }
+    const k = 1024;
+    const dm = decimals <= 0 ? 0 : decimals || 2;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DragAndDropDirective } from './directives/drag-and-drop.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DragAndDropDirective
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AmplifyAngularModule
+    AmplifyAngularModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [
     AmplifyService
